eff582a845090cedea16a1b54fccd242f2315e8b
pengqian05 <374237925@qq.com>
2020-05-15 10:42:39 +0800

    update: excuteString

--- client.js
+++ client.js
@@ -195,8 +195,8 @@ class TrunkedBodyParser {
           this.current = this.WAITING_LENGTH_LINE_END
         }
       } else {
-        this.length *= 10
-        this.length += char.charCodeAt(0) - '0'.charCodeAt(0)
+        this.length *= 16
+        this.length += parseInt(char, 16)
       }
     }
 
---
+++ parse.js
@@ -0,0 +1,7 @@
+module.exports.parseHTML = function parseHTML(html){
+  let state = data;
+  for (let c of html) {
+    state = state(c)
+  }
+  state = state(EOF)
+}
